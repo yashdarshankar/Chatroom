066ea7b95f84606b0b294385ea1148faf13987b3
yashdarshankar <73514654+yashdarshankar@users.noreply.github.com>
2022-06-16 20:14:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,26 @@ var name,message
 
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'keylRl1BIiv1sqSEI'}).base('appHbmOBegV2dFacI');
-var box = document.getElementById("box")
+var box = document.getElementById("box");
+
+
 document.getElementById('result').innerHTML = localStorage.getItem("textvalue");
-//box.scrollTop = box.scrollHeight;
 base('Features').select({
-    // Selecting the first 3 records in All features:
     view: "All features"
 }).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
     records.forEach(function(record) {
         console.log('Retrieved', record.get('name'));
         //box.innerHTML += "<br>" + record.get('message');
         if(record.get('name') == name){
             box.innerHTML += "<br><div class='right'>"+"You : "+record.get('message') +"</div>"
+            box.scrollTop = box.scrollHeight;
         }
         else{
             box.innerHTML += "<br><div class='left'>"+record.get('name')+" : "+record.get('message') +"</div>"
+            box.scrollTop = box.scrollHeight;
         }
-        
-        
-        
     });
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
      fetchNextPage();
 
 }, function done(err) {
@@ -37,10 +31,7 @@ var addbtn = document.getElementById("addbtn");
 addbtn.addEventListener('click', ()=>{
     console.log("works");
     message = document.getElementById("message").value;
-    //name = document.getElementById("f_name").value;
     name = localStorage.getItem("textvalue");
-    
-    
     base('Features').create([
     {
         "fields": {
@@ -57,34 +48,4 @@ addbtn.addEventListener('click', ()=>{
         console.log(record.getId());
     });
     });
-    
-
 })
-
-/*
-function AddMsg(){
-    console.log("works");
-    message = document.getElementById("message").value;
-    
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: 'keylRl1BIiv1sqSEI'}).base('appHbmOBegV2dFacI');
-
-    base('Features').create([
-    {
-        "fields": {
-            "name": name,
-            "message": message
-        }
-    }
-    ], function(err, records) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    records.forEach(function (record) {
-        console.log(record.getId());
-    });
-    });
-
- 
-}*/

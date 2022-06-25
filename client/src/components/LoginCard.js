930ffde79052e6afd780ddcc33b38b73501ed8bf
govind-deshmukh <govinddeshmukh2001@gmail.com>
2022-06-25 12:02:10 +0530

    this is responsive now

--- client/src/components/LoginCard.js
+++ client/src/components/LoginCard.js
@@ -1,27 +1,16 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 
-import './style/cards.css'
+
+// import './style/cards.css'
+
 export default function LoginCard() {
   return (
-    <div className='shadow-lg'>
-    <Card >
-      <CardMedia
-        component="img"
-        height="250"
-        image={require('./images/LoginRegisterCardBg.jpg')}
-        alt="best bg Image"
-      />
-      <CardContent>
-        <div className=' CardHeading text-center'>
-        <Typography gutterBottom variant="h5" component="div">
-          Login User
-        </Typography>
-        </div>
-        <div className='container w-50'>
+    
+      <div class="card shadow-lg">
+      <img class="card-img-top" src={require('./images/LoginRegisterCardBg.jpg')} alt="bgimage"/>
+      <div class="card-body">
+        <h5 class="card-title text-center">Register User</h5>
+        <div className='w-100'>
           <form>
             <div className="mb-3">
               <label for="exampleInputEmail1" className="form-label">Username</label>
@@ -34,11 +23,11 @@ export default function LoginCard() {
             <button type="submit" className="btn btn-primary w-100 mb-5">Submit</button>
           </form>
         </div>
-
-      </CardContent>
-
-    </Card>
+      </div>
     </div>
+   
+
+
 
   );
 }
--- client/src/components/RegisterCard.js
+++ client/src/components/RegisterCard.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+
 
 
 import './style/cards.css'
@@ -10,60 +7,87 @@ import './style/cards.css'
 export default function RegisterCard() {
   return (
 
-    // name = request.form['name']
-    // email = request.form['email']
+    //  name = request.form['name']
+    //  email = request.form['email']
     // contact = request.form['contact']
-    // username = request.form['username']
-    // password = request.form['password']
+    //  username = request.form['username']
+    //  password = request.form['password']
 
 
-    <div className='shadow-lg'>
-   <Card >
-      <CardMedia
-        component="img"
-        height="250"
-        image={require('./images/LoginRegisterCardBg.jpg')} 
-        alt="green iguana"
-      />
-      <CardContent>
-      <div className='text-center CardHeading'>
-      <Typography gutterBottom variant="h5" component="div">
-          Register User
-        </Typography>
-      </div>
-
-        <div className='container w-50'>
+      <div class="card shadow-lg">
+      <img class="card-img-top" src={require('./images/LoginRegisterCardBg.jpg')} alt="bgimage"/>
+      <div class="card-body ">
+        <h5 class="card-title text-center">Register User</h5>
+        <div className='w-100'>
         <form>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Name</label>
-            <input type="text" className="form-control" name="name" aria-describedby="emailHelp"/>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Email</label>
-            <input type="email" className="form-control" name="email" aria-describedby="emailHelp"/>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Contact No</label>
-            <input type="number" className="form-control" name="contact" aria-describedby="emailHelp"/>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Create Username</label>
-            <input type="text" className="form-control" name='username'/>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Create Password</label>
-            <input type="password" className="form-control" name='password'/>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
-            <input type="password" className="form-control" name='confpassword'/>
-          </div>
-          <button type="submit" className="btn btn-primary w-100 mb-5">Submit</button>
-        </form>
+             <div className="mb-3">
+               <label className="form-label">Name</label>
+               <input type="text" className="form-control" name="name" aria-describedby="emailHelp"/>
+             </div>
+             <div className="mb-3">
+               <label className="form-label">Email</label>
+               <input type="email" className="form-control" name="email" aria-describedby="emailHelp"/>
+             </div>
+             <div className="mb-3">
+               <label className="form-label">Contact No</label>
+               <input type="number" className="form-control" name="contact" aria-describedby="emailHelp"/>
+             </div>
+             <div className="mb-3">
+               <label className="form-label">Create Username</label>
+               <input type="text" className="form-control" name='username'/>
+             </div>
+             <div className="mb-3">
+               <label className="form-label">Create Password</label>
+               <input type="password" className="form-control" name='password'/>
+             </div>
+             <div className="mb-3">
+               <label className="form-label">Confirm Password</label>
+               <input type="password" className="form-control" name='confpassword'/>
+             </div>
+             <button type="submit" className="btn btn-primary w-100 mb-5">Submit</button>
+           </form>
         </div>
-      </CardContent>
-    </Card>
+      </div>
     </div>
+   
+
+    //  <div className='shadow-lg'>
+    //    <div class="card">
+    //      <img class="card-img-top" src={require('./images/LoginRegisterCardBg.jpg')} alt="bgimage"/>
+    //      <div class="card-body">
+    //        <h5 class="card-title text-center">Login</h5>
+    //        <div className='w-100'>
+    //        <form>
+    //          <div className="mb-3">
+    //            <label className="form-label">Name</label>
+    //            <input type="text" className="form-control" name="name" aria-describedby="emailHelp"/>
+    //          </div>
+    //          <div className="mb-3">
+    //            <label className="form-label">Email</label>
+    //            <input type="email" className="form-control" name="email" aria-describedby="emailHelp"/>
+    //          </div>
+    //          <div className="mb-3">
+    //            <label className="form-label">Contact No</label>
+    //            <input type="number" className="form-control" name="contact" aria-describedby="emailHelp"/>
+    //          </div>
+    //          <div className="mb-3">
+    //            <label className="form-label">Create Username</label>
+    //            <input type="text" className="form-control" name='username'/>
+    //          </div>
+    //          <div className="mb-3">
+    //            <label className="form-label">Create Password</label>
+    //            <input type="password" className="form-control" name='password'/>
+    //          </div>
+    //          <div className="mb-3">
+    //            <label className="form-label">Confirm Password</label>
+    //            <input type="password" className="form-control" name='confpassword'/>
+    //          </div>
+    //          <button type="submit" className="btn btn-primary w-100 mb-5">Submit</button>
+    //        </form>
+    //      </div>
+    //    </div>
+    //  </div>
+
  
   );
 }
